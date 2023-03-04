cfd7a7e7f688786d889eaafb7c1bb208ed060418
Anshuman Shukla <Anshumanshukla46@gmail.com>
2023-03-04 22:30:08 +0530

    not uploading api key :)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react'
 import "./App.css"
 import Navbar from './components/Navbar'
 import NewsContent from './components/NewsContent/NewsContent'
-import apiKey from './data/config';
 import Footer from "./components/footer/Footer"
 
 function App() {
@@ -16,12 +15,15 @@ function App() {
 
 
   // api request using axios
+  /** 
+   * 
+   * THIS IS NOT GOING FOR DEPLOYING AS WE HAVE NOT TAKING THE BUSINESS SUBCRIPTION  */
   const newApi = async () => {
     try {
 
       // adding with pages 
       const news = await axios.get
-        (`https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}&category=${category}&pageSize=${loadMore}`);
+        (`https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.REACT_APP_API_KEY}&category=${category}&pageSize=${loadMore}`);
 
       setNewsArray(news.data.articles)
       setNewsResults(news.data.totalResults)
@@ -31,7 +33,32 @@ function App() {
     }
   }
 
-  // console.log(newsArray);
+
+
+  // it is done as backend and frontend is not on the same server : cost_error
+
+  // const newApi = async () => {
+  //   try {
+
+  //     // now this proxy url to avoide cost error
+  //     // but now using this it will not work if not deployed and also not always in local host as well
+  //     const proxyUrl = "https://cors.anywhere.herokuapp.com/"
+
+
+
+  //     // adding with pages 
+  //     const news = await axios.get
+  //       (`https://${proxyUrl}newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}&category=${category}&pageSize=${loadMore}`);
+
+  //     setNewsArray(news.data.articles)
+  //     setNewsResults(news.data.totalResults)
+
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }
+
+
 
   useEffect(() => {
     newApi()
--- src/data/config.js
+++
@@ -1,3 +0,0 @@
-const apiKey = "b13b1e2e14114a70b9f6d3a319b2bb97"
-
-export default apiKey
